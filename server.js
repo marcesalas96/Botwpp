7333af686420a2f6c61f81a966464dc3722aa307
marcesalas96 <marcesalas1996@gmail.com>
2022-12-02 22:23:38 -0300

    Update a bdd al cancelar viaje

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const { Client, LocalAuth, Buttons } = require('whatsapp-web.js')
 const qrcode = require('qrcode-terminal')
 const saludoTiempo = require('./util/saludoTiempo')
 const demora = require('./util/demora')
-const connection = require('./Db/config')
+const mysql = require("mysql2/promise")
+require("dotenv").config()
 
 //bdd e inicializacion cliente chat
 const client = new Client({
@@ -18,57 +19,76 @@ client.on('ready', () => {
 
 })
 client.initialize();
-connection.connect()
 
 //declaración de variables globales
 const regexDire1 = new RegExp(/[A-Za-z]+ [0-9]+/)
 const regexDire2 = new RegExp(/[0-9]+ [A-Za-z]+/)
 const regexName = new RegExp(/[A-ZÄËÏÖÜÁÉÍÓÚÂÊÎÔÛÀÈÌÒÙ][a-zäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙñ]+ [A-ZÄËÏÖÜÁÉÍÓÚÂÊÎÔÛÀÈÌÒÙ][a-zäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙñ]+/g)
-let direccion;
+let direcciones = []
 let nombre;
-const btnConfirmar = new Buttons("¿Desea confirmar su móvil? 👇",[{body: "CONFIRMAR"},{body: "CANCELAR"}])
-const btnTipoViaje = new Buttons(`👋 ${saludoTiempo()}, seleccioná tu tipo de viaje.`,[{body: "INMEDIATO"},{body: "PROGRAMADO"}])
-const btnOperadora = new Buttons("Si necesitas comunicarte con la operadora hace click en el siguiente botón 👇.", [{body: "OPERADORA"}])
+const btnConfirmar = new Buttons("¿Desea confirmar su móvil? 👇", [{ body: "CONFIRMAR" }, { body: "CANCELAR" }])
+const btnTipoViaje = new Buttons(`👋 ${saludoTiempo()}, seleccioná tu tipo de viaje.`, [{ body: "INMEDIATO" }, { body: "PROGRAMADO" }])
+const btnOperadora = new Buttons("Si necesitas comunicarte con la operadora hace click en el siguiente botón 👇.", [{ body: "OPERADORA" }])
 let numerosEnAtencion = []
 let contador = 0
 //cliente escuchando mensajes
-client.on('message',async message => {
+client.on('message', async message => {
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    })
     if ((message.id.remote.includes("@c") && !numerosEnAtencion.find(numero => numero.numero === message.from)) && (message.type === "chat" || message.type === "buttons_response")) {
-        if (message.body.toLowerCase().includes("hol") || message.body.toLowerCase().includes("bueb") || message.body.toLowerCase().includes("buen ") || message.body.toLowerCase().includes("buena") || message.body.toLowerCase().includes("necesit") || message.body.toLowerCase().includes("manda") || message.body.toLowerCase().includes("quiero") || message.body.toLowerCase().includes("mánda") || message.body.toLowerCase().includes("buenos") || message.body.toLowerCase().includes("necesito") || message.body.toLowerCase().includes("remis") || message.body.toLowerCase().includes("solicito") || message.body.toLowerCase().includes("enviame") || message.body.toLowerCase().includes("movil")|| message.body.toLowerCase().includes("auto")){
+        if (message.body.toLowerCase().includes("hol") || message.body.toLowerCase().includes("bueb") || message.body.toLowerCase().includes("buen ") || message.body.toLowerCase().includes("buena") || message.body.toLowerCase().includes("necesit") || message.body.toLowerCase().includes("manda") || message.body.toLowerCase().includes("quiero") || message.body.toLowerCase().includes("mánda") || message.body.toLowerCase().includes("buenos") || message.body.toLowerCase().includes("necesito") || message.body.toLowerCase().includes("remis") || message.body.toLowerCase().includes("solicito") || message.body.toLowerCase().includes("enviame") || message.body.toLowerCase().includes("movil") || message.body.toLowerCase().includes("auto") || message.body.toLowerCase().includes("móvil")) {
             nombre = message._data.notifyName
             if (nombre.match(regexName)) {
-                const btnTipoViajeName = new Buttons(`👋 ${saludoTiempo()} *${nombre.match(regexName)[0]}*, seleccioná tu tipo de viaje 👇`,[{body: "INMEDIATO"},{body: "PROGRAMADO"}])
+                const btnTipoViajeName = new Buttons(`👋 ${saludoTiempo()} *${nombre.match(regexName)[0]}*, seleccioná tu tipo de viaje 👇`, [{ body: "INMEDIATO" }, { body: "PROGRAMADO" }])
                 client.sendMessage(message.from, btnTipoViajeName)
             }
             else {
                 client.sendMessage(message.from, btnTipoViaje)
             }
         }
-        else if(message.body === "INMEDIATO"){
+        else if (message.body.toLowerCase() === "inmediato") {
             client.sendMessage(message.from, "Ingresá la dirección donde queres tu móvil en *UN SOLO MENSAJE.* 👇\n*Calle + número* (Por ejemplo: *Belgrano 2204*).")
         }
         else if (regexDire1.test(message.body) || regexDire2.test(message.body)) {
-            direccion = message.body
+            direcciones.push({ mensaje: message.body, numero: message.from })
             message.reply(`⚠️ Su móvil tiene ${demora()} de demora.`)
             client.sendMessage(message.from, btnConfirmar)
-            contador++
-            connection.query(`INSERT INTO mensajes VALUES(${contador},'${direccion}')`)
-            
         }
         else if (message.body.toLowerCase().includes("cancel") || Number(message.body) === 2) {
-            connection.query(`DELETE FROM mensajes WHERE id = ${contador}`)
-            contador--
-            client.sendMessage(message.from, "Su móvil fue cancelado ❌\nGracias por comunicarse con Profesional Remis 🚕")
-            client.sendMessage(message.from, btnOperadora)
+            direcciones = direcciones.filter((direccion) => !(direccion.numero === message.from))
+            const numeroEnBD = await connection.query(`SELECT id, direccion, fecha FROM viajes WHERE telefono= '${message.from}' ORDER BY id DESC LIMIT 1`)
+            if(numeroEnBD[0].length === 0){
+                client.sendMessage(message.from, "Su viaje fue cancelado ❌\nGracias por comunicarse con Profesional Remis 🚕")
+                client.sendMessage(message.from, btnOperadora)
+            }
+            else{
+                await connection.query(`UPDATE viajes SET estado='CANCELADO' where id = ${numeroEnBD[0][0].id}`)
+                client.sendMessage(message.from, "Su viaje fue cancelado ❌\nGracias por comunicarse con Profesional Remis 🚕")
+                client.sendMessage(message.from, btnOperadora)
+            }
         }
         else if (message.body.toLowerCase().includes("confirm") || Number(message.body) === 1) {
+            contador++
+            const horaDeCarga = new Date().toLocaleString("zu-ZA")
+            const direccionBD = direcciones.filter(direccion => direccion.numero === message.from)
+            direcciones = direcciones.filter((direccion) => !(direccion.numero === message.from))
+            if (direccionBD.length > 1) {
+                await connection.query(`INSERT INTO viajes (direccion, fecha, telefono) VALUES('${direccionBD[direccionBD.length - 1].mensaje}', '${horaDeCarga}', '${message.from}')`)
+            }
+            else {
+                await connection.query(`INSERT INTO viajes (direccion, fecha, telefono) VALUES('${direccionBD[0].mensaje}', '${horaDeCarga}', '${message.from}')`)
+            }
             client.sendMessage(message.from, "Su móvil va en camino ☑️\nGracias por comunicarse con Profesional Remis 🚕")
         }
-        else if (message.body.toLowerCase().includes("gracias") || message.body.toLowerCase().includes("ok") || message.body.toLowerCase() === "bueno"){
+        else if (message.body.toLowerCase().includes("gracias") || message.body.toLowerCase().includes("ok") || message.body.toLowerCase() === "bueno") {
             client.sendMessage(message.from, "Gracias por comunicarse con Profesional Remis 🚕!")
         }
-        else if(message.body.toLowerCase() === "operadora" || message.body.toLowerCase() === "programado") {
-            numerosEnAtencion.push({numero: message.from, horaDeEntrada: new Date().getTime()})
+        else if (message.body.toLowerCase() === "operadora" || message.body.toLowerCase() === "programado") {
+            numerosEnAtencion.push({ numero: message.from, horaDeEntrada: new Date().getTime() })
             client.sendMessage(message.from, "Aguardá un momento, la operadora te escribirá en unos minutos...⏳")
             await (await message.getChat()).markUnread()
         }
@@ -80,6 +100,6 @@ client.on('message',async message => {
     }
 })
 
-setInterval(()=> {
+setInterval(() => {
     numerosEnAtencion = numerosEnAtencion.filter(numero => ((new Date().getTime() - numero.horaDeEntrada) <= 600000))
-},60000)
+}, 60000)
